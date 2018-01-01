27837004fcb98a7cf5c9851799a161ebf5abeac7
Manuel Huber <manuel.h87@gmail.com>
2018-01-01 11:34:09 +0100

    Add entrypoints for docker-inside and docker-inside-setup

--- setup.py
+++ setup.py
@@ -19,10 +19,18 @@ setup(
                  "License :: OSI Approved :: MIT License",
                  "Programming Language :: Python :: 3 :: Only",
                  "Topic :: System :: Systems Administration"],
-    packages=['dockerinside'],
+    packages=['dockerinside', 'dockerinside.setup'],
     entry_points={
-        'console_scripts':
-            ['din = dockerinside.__init__:main']},
+        'console_scripts': [
+            'din = dockerinside.__init__:main',
+            'docker-inside = dockerinside.__init__:main',
+            'docker_inside = dockerinside.__init__:main',
+            'dockerinside = dockerinside.__init__:main',
+            'din-setup = dockerinside.setup.__init__:setup_main',
+            'docker-inside-setup = dockerinside.setup.__init__:setup_main',
+            'docker_inside_setup = dockerinside.setup.__init__:setup_main',
+        ]
+    },
     install_requires=["argparse>=1.4.0",
                       "argcomplete>=1.4.1",
                       "blessings>=1.6",
